f1d0cae62845a3d8c4829af2ea3f0d37d6790cf6
TechLounge777 <monika.barto@o2.pl>
2019-02-23 11:07:35 +0100

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import "./App.css";
 import Countdown from './Countdown';
 
 class App extends Component {
@@ -8,23 +9,24 @@ class App extends Component {
         this.state = {
             events: [
                 { id: 0, name: "breakfast", time: "7:00" },
-                { id: 1, name: "lunch", time: "13:00" }
-                
+                { id: 1, name: "lunch", time: "13:00" },
+                { id: 2, name: "dinner", time: "18:00" }
             ]
         }
     }
 
     render() {
-        return (
-            <div>
-                <Countdown name="breakfast" time="7:00" />
-                <Countdown name="lunch" time="13:00" />
-            </div>
-       )
+        const events = this.state.events.map(el => {
+            return <Countdown key={el.id} name={el.name} time={el.time} />;
+        });
+        return  <div className="app">{events}</div>;
     }
-};
+}
 
 export default App;
+
+
+
 
 
 
